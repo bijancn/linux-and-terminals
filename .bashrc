ba3c9ff5f65f449d7aaac4246ecd8f8356f80d92
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2017-07-17 17:18:40 +0200

    Improve setup script [0]

--- .bashrc
+++ .bashrc
@@ -1,1 +1,2 @@
-alias la=la -la
+alias la='la -la'
+export PS1='\[\e[00;34m\]\u\[\e[02;37m\]@\[\e[01;31m\]\h:\[\e[01;34m\] \w \[\e[00m\]\n $ '
--- setup-environment.sh
+++ setup-environment.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
-mv ~/.bashrc ~/.bashrc.backup
+#!/bin/bash
+mv .bashrc .bashrc.backup
 touch .bashrc
-mv ~/.zshrc ~/.zshrc.backup
+mv .zshrc .zshrc.backup
 touch .zshrc
 cat > .bashrc << EOF
+alias la='la -la'
 export PS1='\[\e[00;34m\]\u\[\e[02;37m\]@\[\e[01;31m\]\h:\[\e[01;34m\] \w \[\e[00m\]\n $ '
 EOF
 cp .bashrc .zshrc
+if [[ $SHELL == *zsh* ]]; then
+  echo "Looks like you are using ZSH"
+  source .zshrc
+else
+  echo "Looks like you are using BASH"
+  source .bashrc
+fi
